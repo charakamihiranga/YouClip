8374dbadf9cfcaef37cef94f85106b84aae57032
CharakaMihiranga <charakavmihiranga@gmail.com>
2024-07-23 17:11:34 +0530

    added the bookmark button to the yt-Player

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
+// Listen for tab updates
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Check if the tab URL contains "youtube.com"
+  if (tab.url.includes("youtube.com/watch")) {
+    console.log("YouTube tab detected:", tab.url);
 
-    console.log(`New video loaded: ${urlParameters.get("v")}`);
+    const queryPrameters = tab.url.split("?")[1];
+    const urlParmeters = new URLSearchParams(queryPrameters);
+    
+    console.log(urlParmeters);
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
-      videoId: urlParameters.get("v"),
+      videoId : urlParmeters.get("v")
     });
 
+
+    
   }
-})
+});
--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,54 @@
-(
-  () => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
+(() => {
+  let youtubeLeftControles, youtubePlayer;
+  let currentVideo = "";
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-      const {type, value, videoId } = obj;
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
 
-      if (type === "NEW" ) {
-        currentVideo = videoId;
-        newVideoLoaded();
-      }
-    })
-  })
-();
+    if (type == "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
+  });
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists = document.getElementsByClassName("bookmarkBtn")[0];
+
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+  
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.style.height = "26px";
+      bookmarkBtn.style.width = "32px";
+      bookmarkBtn.style.marginLeft = "54%";
+      bookmarkBtn.style.marginTop = "12px";  
+      bookmarkBtn.style.cursor = "pointer";   
+      bookmarkBtn.className = "ytp-button bookmarkBtn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
+  
+      const youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+      const youtubePlayer = document.getElementsByClassName("video-stream")[0];
+  
+      youtubeLeftControls.appendChild(bookmarkBtn);
+
+      
+
+
+      //hover styles for the bookmark button
+
+      const style = document.createElement('style');
+      style.innerHTML = `
+          .bookmarkBtn:hover {
+              transform: scale(1.2);
+          }
+      `;
+      document.head.appendChild(style);
+
+  }
+  
+
+
+  }
+
+  newVideoLoaded();
+})();
